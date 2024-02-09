ba39a9d8ec5fa2d957b203ffa22fe68887272989
Anderson Raphael Ferreira <phaell.anderson09gmail.com>
2024-02-09 09:12:59 -0300

    fix: alteradas variáveis de ambiente para o arquivo .env

--- src/infra/database/dataSource.ts
+++ src/infra/database/dataSource.ts
@@ -6,11 +6,11 @@ dotenv.config();
 export function getConfig() {
     return {
         type: 'postgres',
-        host: 'postgresdb',
+        host: process.env.DB_HOST,
         port: 5432,
-        username: 'postgres',
-        password: 'docker',
-        database: 'doopay',
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
         synchronize: false,
         migrations: ['src/infra/database/migrations/**/*.ts'],
         entities: ['src/modules/**/entity/*.ts'],
